c977f9519ce937cdc1875c3346d133ede79a91b2
Daniel Corcoran <danielc.92@outlook.com>
2019-03-01 10:10:36 +1100

    Added offices to data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, render_template
 from flask_restplus import Resource, Api, fields
 import json
 
@@ -8,21 +8,33 @@ api = Api(app,
           description="A test restful-api by Daniel.",
           validate=True)
 
-an_employee = api.model('Employee', 
+
+an_employee = api.model('Employee',
                         {
                             "full-name": fields.String(required=True, description="Full name of employee."),
-                             "occupation": fields.String(required=True, description="Job title in company."), 
-                             "hours-worked": fields.Integer(required=True, description="Number of hours worked for company.")
+                            "occupation": fields.String(required=True, description="Job title in company."),
+                            "hours-worked": fields.Integer(required=True, description="Number of hours worked for company.")
                         })
 
-with open('./employees.json', 'r') as f:
-    employees = json.load(f)
+
+an_office = api.model('Office',
+                      {
+                          "name": fields.String(required=True, description="Office name."),
+                          "address": fields.String(required=True, description="Office address."),
+                          "postcode": fields.Integer(required=True, description="Office postcode.")
+                      })
+
+
+# Read in Data
+with open('./data.json', 'r') as f:
+    data = json.load(f)
+
 
 @api.route('/export')
 class Export(Resource):
     def get(self):
-        with open('./employees.json', 'w') as f:
-            json.dump(employees, f, sort_keys=True, indent=4)
+        with open('./data2.json', 'w') as f:
+            json.dump(data, f, sort_keys=True, indent=4)
             f.close()
         return {"message": "Data saved locally."}
 
@@ -30,12 +42,14 @@ class Export(Resource):
 @api.route('/employees')
 class Employee(Resource):
     def get(self):
-        return employees
+        return data['employees']
+
 
 @api.route('/employees/<string:emp_id>')
 class Employee(Resource):
     def get(self, emp_id):
-        result_set = [e for e in employees if emp_id == e["id"]]
+
+        result_set = [e for e in data['employees'] if emp_id == e['id']]
 
         if len(result_set) > 0:
             # Return the first result from the list if results are found.
@@ -46,15 +60,16 @@ class Employee(Resource):
 
     @api.expect(an_employee)
     def post(self, emp_id):
-        result_set = [e for e in employees if emp_id == e["id"]]
+
+        result_set = [e for e in data['employees'] if emp_id == e["id"]]
 
         if len(result_set) > 0:
             return {"message": "ID already exists. Choose another one"}
         else:
             post_data = api.payload
             post_data["id"] = emp_id
-            employees.append(post_data)
-            return employees
+            data['employees'].append(post_data)
+            return data['employees']
 
 if __name__ == '__main__':
     app.run(debug=True)
